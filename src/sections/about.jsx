050b391f6ed8e9dc869d204bb0690328be3d914f
Gian Mateus <gian.m.silver@gmail.com>
2025-01-01 18:12:03 -0300

    Refatorar layout da seção "about" para melhorar responsividade
    - Adicionado container com largura máxima e centralização
    - Implementado layout flexível para dispositivos maiores
    - Ajustado espaçamento e alinhamento dos elementos

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -1,55 +1,57 @@
 export default function about() {
   return (
-    <section className="bg-amber-800 min-h-screen flex flex-col p-10 rounded-b-3xl">
-      {/* Card */}
-      <div className="p-4 shadow-xl bg-yellow-50 max-h-[70vh] h-fit rounded-3xl max-w-lg">
-        <div>
-          <img
-            className="object-cover rounded-3xl"
-            src="https://img.freepik.com/fotos-gratis/dentista-feminina-com-ferramentas-de-odontologia-isolado_1303-13189.jpg?t=st=1735485394~exp=1735488994~hmac=27ecece484ee1d17a202f94c1d66c59305c5e5927db2f2a7ce0287618a6b9e97&w=740"
-            alt=""
-          />
+    <section className="bg-amber-800 min-h-screen p-10 rounded-b-3xl">
+      <div className="md:max-w-5xl mx-auto flex md:flex-row">
+        {/* Card */}
+        <div className="p-4 shadow-xl bg-yellow-50 max-h-[70vh] h-fit rounded-3xl max-w-lg">
+          <div>
+            <img
+              className="object-cover rounded-3xl"
+              src="https://img.freepik.com/fotos-gratis/dentista-feminina-com-ferramentas-de-odontologia-isolado_1303-13189.jpg?t=st=1735485394~exp=1735488994~hmac=27ecece484ee1d17a202f94c1d66c59305c5e5927db2f2a7ce0287618a6b9e97&w=740"
+              alt=""
+            />
+          </div>
+          <div>
+            <h3 className="text-xl font-bold text-amber-950 mt-2 italic">
+              Dra. Paola Rossigalli
+            </h3>
+            <article className="text-amber-900">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem
+              eaque similique dolor et voluptatibus dicta perspiciatis quis
+              fugit est libero voluptatem quidem saepe voluptate, asperiores
+              nesciunt sed animi illo doloremque?
+            </article>
+          </div>
         </div>
-        <div>
-          <h3 className="text-xl font-bold text-amber-950 mt-2 italic">
-            Dra. Paola Rossigalli
-          </h3>
-          <article className="text-amber-900">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem eaque
-            similique dolor et voluptatibus dicta perspiciatis quis fugit est
-            libero voluptatem quidem saepe voluptate, asperiores nesciunt sed
-            animi illo doloremque?
-          </article>
-        </div>
-      </div>
 
-      {/* Infos */}
-      <div className="p-6 text-amber-100 flex flex-col gap-12 mb-10">
-        <div>
+        {/* Infos */}
+        <div className="p-6 text-amber-100 flex flex-col gap-12 mb-10">
           <div>
-            <h2 className="text-3xl font-bold font-title">
-              Meus ovo bem grande
-            </h2>
+            <div>
+              <h2 className="text-3xl font-bold font-title">
+                Meus ovo bem grande
+              </h2>
+            </div>
+            <article>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+              possimus, veniam consequatur quam qui odit deserunt saepe natus
+              optio ut eligendi voluptatem, sapiente distinctio asperiores
+              assumenda eum nostrum eos doloribus.
+            </article>
           </div>
-          <article>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-            possimus, veniam consequatur quam qui odit deserunt saepe natus
-            optio ut eligendi voluptatem, sapiente distinctio asperiores
-            assumenda eum nostrum eos doloribus.
-          </article>
-        </div>
-        <div>
           <div>
-            <h2 className="text-3xl font-bold font-title">
-              Meus ovo bem grande
-            </h2>
+            <div>
+              <h2 className="text-3xl font-bold font-title">
+                Meus ovo bem grande
+              </h2>
+            </div>
+            <article>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+              possimus, veniam consequatur quam qui odit deserunt saepe natus
+              optio ut eligendi voluptatem, sapiente distinctio asperiores
+              assumenda eum nostrum eos doloribus.
+            </article>
           </div>
-          <article>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-            possimus, veniam consequatur quam qui odit deserunt saepe natus
-            optio ut eligendi voluptatem, sapiente distinctio asperiores
-            assumenda eum nostrum eos doloribus.
-          </article>
         </div>
       </div>
     </section>
