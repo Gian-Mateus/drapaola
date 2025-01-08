e4f4b471e58784ea3c35fc1ee7150b7ee6578d6d
Gian Mateus <gian.m.silver@gmail.com>
2025-01-08 12:56:07 -0300

    create section cases

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Header from "./sections/header"
 import About from "./sections/about"
 import Harmonization from "./sections/harmonization"
+import Cases from "./sections/cases"
 function App() {
   return (
     <>
-    <Header />
+      <Header />
       <main className="font-body">
         <About />
         <Harmonization />
+        <Cases />
       </main>
     </>
-
   )
 }
 export default App
--- src/sections/cases.jsx
+++ src/sections/cases.jsx
@@ -1,6 +1,24 @@
 
 export default function Cases() {
     return (
-        <h1>Cases</h1>
+      <section className="flex justify-center w-full ">
+        <div className="bg-amber-200/80 -z-10 m-12 rounded-[40px] p-12">
+          <div className="flex">
+          <h1 className="text-4xl font-title font-bold">Casos e Depoimentos</h1>
+          </div>
+          <div>
+            <div>
+              <div className="mt-10">
+                <h3 className="text-xl font-bold font-title">
+                  Botox
+                </h3>
+              </div>
+              <div className="w-44 h-48 bg-zinc-300 rounded-md ">
+                Imagem
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     )
 }
