caedfea36f214f096f7799a617fb7531550577ca
Gian Mateus <gian.m.silver@gmail.com>
2025-01-08 18:14:30 -0300

    criando cards e inicio do desing

--- src/sections/cases.jsx
+++ src/sections/cases.jsx
@@ -1,20 +1,64 @@
 
 export default function Cases() {
     return (
-      <section className="flex justify-center w-full ">
+      <section className="flex justify-center w-full">
         <div className="bg-amber-200/80 -z-10 m-12 rounded-[40px] p-12">
-          <div className="flex">
-          <h1 className="text-4xl font-title font-bold">Casos e Depoimentos</h1>
+          <div className="flex flex-col gap-4">
+            <h1 className="text-4xl font-title font-bold">Casos e Depoimento
+            </h1>
+            <p>Alguns casos de antes e depois, demonstrando o quanto a harmonização faz diferença na autoestima das pessoas.</p>
           </div>
-          <div>
-            <div>
-              <div className="mt-10">
+          <div className="grid grid-flow-col gap-12 overflow-x-scroll auto-cols-max max-w-[60vw]">
+            <div className="mt-6 shadow-md rounded-md overflow-hidden min-w-max">
+              <div className="w-full h-48 bg-zinc-300">
+                
+              </div>
+              <div className="mt-3 border-t-2 border-amber-700 py-2 mx-2">
                 <h3 className="text-xl font-bold font-title">
                   Botox
                 </h3>
+                <p>
+                  bla bla bla bla bla bla bla bla bla bla
+                </p>
               </div>
-              <div className="w-44 h-48 bg-zinc-300 rounded-md ">
-                Imagem
+            </div>
+            <div className="mt-6 shadow-md rounded-md overflow-hidden min-w-max">
+              <div className="w-full h-48 bg-zinc-300">
+                
+              </div>
+              <div className="mt-3 border-t-2 border-amber-700 py-2 mx-2">
+                <h3 className="text-xl font-bold font-title">
+                  Botox
+                </h3>
+                <p>
+                  bla bla bla bla bla bla bla bla bla bla
+                </p>
+              </div>
+            </div>
+            <div className="mt-6 shadow-md rounded-md overflow-hidden min-w-max">
+              <div className="w-full h-48 bg-zinc-300">
+                
+              </div>
+              <div className="mt-3 border-t-2 border-amber-700 py-2 mx-2">
+                <h3 className="text-xl font-bold font-title">
+                  Botox
+                </h3>
+                <p>
+                  bla bla bla bla bla bla bla bla bla bla
+                </p>
+              </div>
+            </div>
+            <div className="mt-6 shadow-md rounded-md overflow-hidden min-w-max">
+              <div className="w-full h-48 bg-zinc-300">
+                
+              </div>
+              <div className="mt-3 border-t-2 border-amber-700 py-2 mx-2">
+                <h3 className="text-xl font-bold font-title">
+                  Botox
+                </h3>
+                <p>
+                  bla bla bla bla bla bla bla bla bla bla
+                </p>
               </div>
             </div>
           </div>
