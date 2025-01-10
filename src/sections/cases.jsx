41e80a15095455fd401bd29a6043d418f60d0b02
Gian Mateus <gian.m.silver@gmail.com>
2025-01-09 22:21:47 -0300

    Refatorar seção de Casos e Depoimentos
    - Atualizar dependências no package-lock.json
    - Reestruturar o componente Cases para melhor organização e reutilização
    - Implementar geração dinâmica de cards usando um loop
    - Melhorar o layout e a responsividade da seção
    - Adicionar rolagem suave e interação de toque para os cards

--- src/sections/cases.jsx
+++ src/sections/cases.jsx
@@ -1,68 +1,42 @@
-
 export default function Cases() {
-    return (
-      <section className="flex justify-center w-full">
-        <div className="bg-amber-200/80 -z-10 m-12 rounded-[40px] p-12">
-          <div className="flex flex-col gap-4">
-            <h1 className="text-4xl font-title font-bold">Casos e Depoimento
-            </h1>
-            <p>Alguns casos de antes e depois, demonstrando o quanto a harmonização faz diferença na autoestima das pessoas.</p>
-          </div>
-          <div className="grid grid-flow-col gap-12 overflow-x-scroll auto-cols-max max-w-[60vw]">
-            <div className="mt-6 shadow-md rounded-md overflow-hidden min-w-max">
-              <div className="w-full h-48 bg-zinc-300">
-                
-              </div>
-              <div className="mt-3 border-t-2 border-amber-700 py-2 mx-2">
-                <h3 className="text-xl font-bold font-title">
-                  Botox
-                </h3>
-                <p>
-                  bla bla bla bla bla bla bla bla bla bla
-                </p>
-              </div>
-            </div>
-            <div className="mt-6 shadow-md rounded-md overflow-hidden min-w-max">
-              <div className="w-full h-48 bg-zinc-300">
-                
-              </div>
-              <div className="mt-3 border-t-2 border-amber-700 py-2 mx-2">
-                <h3 className="text-xl font-bold font-title">
-                  Botox
-                </h3>
-                <p>
-                  bla bla bla bla bla bla bla bla bla bla
-                </p>
-              </div>
-            </div>
-            <div className="mt-6 shadow-md rounded-md overflow-hidden min-w-max">
-              <div className="w-full h-48 bg-zinc-300">
-                
-              </div>
-              <div className="mt-3 border-t-2 border-amber-700 py-2 mx-2">
-                <h3 className="text-xl font-bold font-title">
-                  Botox
-                </h3>
-                <p>
-                  bla bla bla bla bla bla bla bla bla bla
-                </p>
-              </div>
-            </div>
-            <div className="mt-6 shadow-md rounded-md overflow-hidden min-w-max">
-              <div className="w-full h-48 bg-zinc-300">
-                
-              </div>
-              <div className="mt-3 border-t-2 border-amber-700 py-2 mx-2">
-                <h3 className="text-xl font-bold font-title">
-                  Botox
-                </h3>
-                <p>
-                  bla bla bla bla bla bla bla bla bla bla
-                </p>
-              </div>
-            </div>
-          </div>
+  const dataCards = {
+    title: "Botox",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ex, maiores nihil expedita magni quaerat aliquid voluptatum explicabo a consequatur repellendus praesentium laudantium nam, molestiae nesciunt cumque aperiam nemo dolorum?",
+    imgPath: "...",
+  };
+
+  const e = [];
+  for (let i = 0; i < 10; i++) {
+    e.push(
+      <div
+        className="mt-6 shadow-md rounded-md overflow-hidden max-w-sm"
+        key={i}
+      >
+        <div className="w-full h-48 bg-zinc-300"></div>
+        <div className="mt-3 border-t-2 border-amber-700 py-2 mx-2">
+          <h3 className="text-xl font-bold font-title">{dataCards.title}</h3>
+          <p>{dataCards.content}</p>
         </div>
-      </section>
-    )
+      </div>
+    );
+  }
+
+  return (
+    <section className="flex justify-center w-full bg-amber-200/80 overflow-hidden">
+      <div className="my-24">
+        <div className="flex flex-col gap-4 mx-24">
+          <h1 className="text-4xl font-title font-bold">Casos e Depoimentos</h1>
+          <p>
+            Alguns casos de antes e depois, demonstrando o quanto a harmonização
+            faz diferença na autoestima das pessoas.
+          </p>
+        </div>
+        <div className="grid grid-flow-col gap-6 overflow-x-scroll auto-cols-max w-screen md:max-w-screen-md p-10 scroll-smooth touch-pan-x cursor-grab">
+          {/* Cards */}
+          {e}
+        </div>
+      </div>
+    </section>
+  );
 }
