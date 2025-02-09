e2022b879c7b06e8ec5243c4ddd6c8ab73b3648c
Gian Mateus <gian.m.silver@gmail.com>
2025-02-09 13:07:38 -0300

    mudei nada

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { motion } from "motion/react";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "motion/react";
+import Header from "./sections/header";
 import About from "./sections/about";
 import Harmonization from "./sections/harmonization";
 import Cases from "./sections/cases";
 import Footer from "./sections/footer";
-import Header from "./sections/header";
 
-// const { scrollY, scrollProgress } = useScroll()
-function App() {
-  return (
-    <motion.div>
-      <main className="font-body relative z-10">
-        <Header />
-        <About />
-        <Harmonization />
-        <Cases />
-      </main>
-      <Footer />
-    </motion.div>
-  );
-}
-export default App;
+export default function App() {
+  const containerRef = useRef(null);
+  
+return (
+  <>
+    <Header />
+    <main className="font-body" ref={containerRef}>
+      <About />
+      <Harmonization />
+      <Cases />
+    </main>
+    <Footer />
+    </>
+);
+};
